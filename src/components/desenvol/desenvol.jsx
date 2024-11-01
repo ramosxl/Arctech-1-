cca5163618d990841e8da7dc264020d425ee54b5
Duarte <00001104044079sp@al.educacao.sp.gov.br>
2024-10-31 22:01:37 -0300

    estava fazendo o roda pé mas me irritei vou continuar faer os slides não estava me estressando

---
+++ src/components/desenvol/desenvol.jsx
@@ -0,0 +1,11 @@
+import './desenvol.scss'
+
+export default function Desenvolvimento () {
+    return(
+    <div className='Desenvolvimento'>
+        <p>A Arctech, é especializada em desenvolver sites para autônomos  e grandes empresas com o objetivo de ajudá-los a  expandir e dar um ar moderno e atual para sua empresa, assim, atraindo mais clientes e deixando seus produtos e serviços mais acessíveis para todos independentemente da distância. </p>
+        <hr/>
+    </div>
+    
+    )
+}
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,13 +4,17 @@ import './Footer.scss';
 export default function Footer ()  {
 return(
   <footer className="footer">
+    <div className='Contact'>contato@arctech.com.br
+      <br />
+    (11) 98765-4321</div>
     <div className="footer__logo">ArcTech</div>
-    <p>Copyright © 2024 - ArcTech</p>
     <div className="footer__social">
       <a href="#">Facebook</a>
       <a href="#">Instagram</a>
       <a href="#">LinkedIn</a>
     </div>
+    
+    <p>Copyright © 2024 - ArcTech</p>
   </footer>
   )
 }
